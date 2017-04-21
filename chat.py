0a5463e0bbb97fa8c9ec50e406f24ffb40b5b63b
Will Tribble <wltribble@gmail.com>
2017-04-21 00:56:02 -0500

    Updated database support for deployment

--- chat.py
+++ chat.py
@@ -2,12 +2,28 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 from flask_sqlalchemy import SQLAlchemy
 
+import psycopg2
+import urlparse
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'apparently_i_need_this'
 socket = SocketIO(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/flask_chat'
-database = SQLAlchemy(app)
+# Database setup
+try:
+	urlparse.uses_netloc.append("postgres")
+	url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+	database = psycopg2.connect(
+	    database=url.path[1:],
+	    user=url.username,
+	    password=url.password,
+	    host=url.hostname,
+	    port=url.port
+	)
+except:
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/flask_chat'
+	database = SQLAlchemy(app)
 
 class History(database.Model):
 	id = database.Column('id', database.Integer, primary_key=True)
